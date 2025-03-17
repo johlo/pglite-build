d5e72a2786844ad92eccdc2fff00d8dfb0092347
Paul m. p. Peny <mail.peny@free.fr>
2025-03-17 22:43:42 +0100

    tests

--- ci-17_4_WASM.sh
+++ ci-17_4_WASM.sh
@@ -52,103 +52,107 @@ END
 
     if [ -f build/postgres/libpgcore.a ]
     then
-        for archive in ${PG_DIST_EXT}/*.tar
-        do
-            echo "    packing $archive"
-            gzip -f -9 $archive
-        done
+        echo found build/postgres/libpgcore.a
+    else
+        echo failed to build libpgcore static
+        exit 58
+    fi
+    
+    for archive in ${PG_DIST_EXT}/*.tar
+    do
+        echo "    packing $archive"
+        gzip -f -9 $archive
+    done
 
-        echo "
+    echo "
 
     *   preparing TS build assets
 
 "
 
-        if [ -d ${WORKSPACE}/pglite/packages/pglite ]
-        then
-            echo "using local pglite tree for release"
-        else
-            git clone --no-tags --depth 1 --single-branch --branch pmp-p/pglite-build17 https://github.com/electric-sql/pglite pglite
-        fi
+popd
+
+if [ -d ${WORKSPACE}/pglite/packages/pglite ]
+then
+    echo "using local pglite tree for release"
+else
+    git clone --no-tags --depth 1 --single-branch --branch pmp-p/pglite-build17 https://github.com/electric-sql/pglite pglite
+fi
         
-        pushd ${WORKSPACE}/pglite
-            # clean
-            [ -d packages/pglite/release ] && rm packages/pglite/release/* packages/pglite/dist/* packages/pglite/dist/*/*
+pushd ${WORKSPACE}/pglite
+    # clean
+    [ -d packages/pglite/release ] && rm packages/pglite/release/* packages/pglite/dist/* packages/pglite/dist/*/*
 
-            # be safe
-            mkdir -p packages/pglite/release packages/pglite/dist
+    # be safe
+    mkdir -p packages/pglite/release packages/pglite/dist
 
-            rmdir packages/pglite/dist/*
+    rmdir packages/pglite/dist/*
 
-            #update
-            mv -vf ${WORKSPACE}/postgresql-${PG_BRANCH}/pglite.* packages/pglite/release/
-            mv -vf ${PG_DIST_EXT}/*.tar.gz packages/pglite/release/
-        popd
+    #update
+    mv -vf ${WORKSPACE}/postgresql-${PG_BRANCH}/pglite.* packages/pglite/release/
+    mv -vf ${PG_DIST_EXT}/*.tar.gz packages/pglite/release/
+popd
 
-        # when outside CI use emsdk node
-        if [ -d /srv/www/html/pglite-web ]
-        then
-            . /opt/python-wasm-sdk/wasm32-bi-emscripten-shell.sh
-        fi
+# when outside CI use emsdk node
+if [ -d /srv/www/html/pglite-web ]
+then
+    . /opt/python-wasm-sdk/wasm32-bi-emscripten-shell.sh
+fi
 
-        echo "
+echo "
     *   compiling TS
-
 "
-        if $CI
-        then
-            pushd build/postgres
-                echo "# packing dev files to /tmp/sdk/libpglite-emsdk.tar.gz"
-                tar -cpRz libpgcore.a pglite.* > /tmp/sdk/libpglite-emsdk.tar.gz
-            popd
+if $CI
+then
+    pushd build/postgres
+        echo "# packing dev files to /tmp/sdk/libpglite-emsdk.tar.gz"
+        tar -cpRz libpgcore.a pglite.* > /tmp/sdk/libpglite-emsdk.tar.gz
+    popd
 
-            pushd pglite
-                npm install -g pnpm vitest
-                pnpm install
-            popd
-        fi
+    pushd pglite
+        npm install -g pnpm vitest
+        pnpm install
+    popd
+fi
 
 
-        pushd pglite
-            if pnpm run ts:build
+pushd pglite
+    if pnpm run ts:build
+    then
+        pushd packages/pglite
+            if $CI
             then
-                pushd packages/pglite
-                    if $CI
-                    then
-                        pnpm vitest tests/basic.test.js || exit 99
-                    fi
-                popd
+                pnpm vitest tests/basic.test.js || exit 99
             fi
         popd
-
-
-        mkdir -p ${PG_DIST_WEB}
-        if [ -d ${PG_DIST_WEB} ]
-        then
-            git restore src/test/Makefile src/test/isolation/Makefile
-
-            echo "# use released files for web test"
-            mkdir -p ${PG_DIST_WEB}/examples
-            cp -r ${WORKSPACE}/pglite/packages/pglite/dist ${PG_DIST_WEB}/
-            cp ${WORKSPACE}/pglite/packages/pglite/examples/{styles.css,utils.js} ${PG_DIST_WEB}/examples/
-            cp -f ${WORKSPACE}/pglite/packages/pglite/release/* ${PG_DIST_WEB}/
-            du -hs ${PG_DIST_WEB}/pglite.*
-
-            if $LOCAL
-            then
-                echo "# backup pglite workfiles"
-                [ -d pglite-wasm ] && cp -R pglite-wasm/* ${WORKSPACE}/pglite-${PG_BRANCH}/
-            else
-                mv ${PG_DIST_WEB} /tmp/web
-            fi
-        fi
-
-        ./runtests.sh
-
-
-    else
-        echo failed to build libpgcore static
-        exit 125
     fi
 popd
+
+
+mkdir -p ${PG_DIST_WEB}
+if [ -d ${PG_DIST_WEB} ]
+then
+
+    pushd postgresql-${PG_BRANCH}
+        git restore src/test/Makefile src/test/isolation/Makefile
+    popd
+    
+    echo "# use released files for web test"
+    mkdir -p ${PG_DIST_WEB}/examples
+    cp -r ${WORKSPACE}/pglite/packages/pglite/dist ${PG_DIST_WEB}/
+    cp ${WORKSPACE}/pglite/packages/pglite/examples/{styles.css,utils.js} ${PG_DIST_WEB}/examples/
+    cp -f ${WORKSPACE}/pglite/packages/pglite/release/* ${PG_DIST_WEB}/
+    du -hs ${PG_DIST_WEB}/pglite.*
+
+    if $LOCAL
+    then
+        echo "# backup pglite workfiles"
+        [ -d pglite-wasm ] && cp -R pglite-wasm/* ${WORKSPACE}/pglite-${PG_BRANCH}/
+    else
+        mv ${PG_DIST_WEB} /tmp/web
+    fi
+fi
+
+./runtests.sh
+
 
